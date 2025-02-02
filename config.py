95a2e6b76fcde0939964f2e90ebb8d3a7899cb02
kingmartinlutar <challengegaming98@gmail.com>
2025-02-03 02:20:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 class Config:
     # Required configurations
-        BOT_TOKEN = os.getenv("BOT_TOKEN")
-            API_ID = int(os.getenv("API_ID", 0))
-                API_HASH = os.getenv("API_HASH")
-                    ADMINS = [int(admin) for admin in os.getenv("ADMINS", "").split(",")]
-                        DB_URI = os.getenv("DB_URI")
-                            DB_NAME = os.getenv("DB_NAME", "vj_bot")
+        BOT_TOKEN = os.getenv("8056263089:AAHMI88zp5GyHzeaQS82BKNzGFzEI_5DAPs")
+            API_ID = int(os.getenv("5969730414", 0))
+                API_HASH = os.getenv("bff60a5f566d9a54c175b0001ba9615b")
+                    ADMINS = [int(admin) for admin in os.getenv("ADMINS", "5969730414").split(",")]
+                        DB_URI = os.getenv("mongodb+srv://lelouchlightlevi:gZRinZeJNXI55Va0@cluster0.n99js.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+                            DB_NAME = os.getenv("DB_NAME", "king_bot")
                                 
                                     # Optional configurations
                                         ERROR_MESSAGE = os.getenv("ERROR_MESSAGE", "false").lower() == "true"
